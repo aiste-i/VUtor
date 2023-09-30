df991707d7427cfc034ba93d8c1a2c668739d4c2
aiste <98818740+aiste-i@users.noreply.github.com>
2023-09-30 10:01:00 +0300

    Add files via upload

---
+++ VUtor/VUtor/Entities/ProfileEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace VUtor.Models
+{
+    public class ProfileEntity
+    {
+        public int ProfileId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public List<TopicEntity> TopicsToTeach { get; set; }
+        public List<TopicEntity> TopicsToLearn { get; set; }
+    }
+}
