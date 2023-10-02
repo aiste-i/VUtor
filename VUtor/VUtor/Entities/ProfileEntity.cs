b19d9a8d46707aaf8d72c73d1adbcba1348ccf64
aiste <98818740+aiste-i@users.noreply.github.com>
2023-10-02 14:21:23 +0300

    Revert "namespace change in entities"

--- VUtor/VUtor/Entities/ProfileEntity.cs
+++ VUtor/VUtor/Entities/ProfileEntity.cs
@@ -1,4 +1,5 @@
-﻿namespace VUtor.Entities
+<<<<<<< HEAD
+﻿namespace VUtor.Models
 {
     public class ProfileEntity
     {
@@ -10,4 +11,17 @@
         public List<TopicEntity> TopicsToLearn { get; set; }
     }
 }
+=======
+﻿namespace VUtor.Models
+{
+    public class ProfileEntity
+    {
+        public int ProfileId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
 
+        public List<TopicEntity> TopicsToTeach { get; set; }
+        public List<TopicEntity> TopicsToLearn { get; set; }
+    }
+}
+>>>>>>> main
--- VUtor/VUtor/Entities/TopicEntity.cs
+++ VUtor/VUtor/Entities/TopicEntity.cs
@@ -1,4 +1,5 @@
-﻿namespace VUtor.Entities
+<<<<<<< HEAD
+﻿namespace VUtor.Models
 {
     public class TopicEntity
     {
@@ -9,4 +10,16 @@
         public List<ProfileEntity> LearningProfiles { get; set; }
     }
 }
+=======
+﻿namespace VUtor.Models
+{
+    public class TopicEntity
+    {
+        public int TopicId { get; set; }
+        public string Title { get; set; }
 
+        public List<ProfileEntity> TeachingProfiles { get; set; }
+        public List<ProfileEntity> LearningProfiles { get; set; }
+    }
+}
+>>>>>>> main
