907be75274e57e8dc4a0c21c755f445cddbc97fc
Ulomei <Gab.Drungilaite@gmail.com>
2023-10-10 21:36:34 +0300

    istryniau komentara

--- VUtor/Program.cs
+++ VUtor/Program.cs
@@ -49,7 +49,7 @@ app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
-/*using (var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
@@ -83,6 +83,6 @@ using (var scope = app.Services.CreateScope())
         await userManager.AddToRoleAsync(user, "Admin");
 
     }
-}*/
+}
 
 app.Run();
