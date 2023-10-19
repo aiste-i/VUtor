ef1cf476c1a9742c57fd39913da0552df699d11f
Ulomei <Gab.Drungilaite@gmail.com>
2023-10-20 02:43:34 +0300

    File page has a modal

---
+++ VUtor/Pages/files.razor.js
@@ -0,0 +1,20 @@
+﻿
+export function showModal(elementID) {
+    if (elementID != null && elementID != undefined) {
+        let modalElement = document.getElementById(elementID);
+        modalElement.style.display = "block";
+        return "ok";
+    } else {
+        return "error";
+    }
+}
+
+export function HideModal(elementID) {
+    if (elementID != null && elementID != undefined) {
+        let modalElement = document.getElementById(elementID);
+        modalElement.style.display = "none";
+        return "ok";
+    } else {
+        return "error";
+    }
+}
